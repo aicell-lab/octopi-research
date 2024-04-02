a24c7e02834d985b6742bcca4924ebbe78d952ea
Songtao Cheng <36098954+cccoolll@users.noreply.github.com>
2024-04-02 15:49:58 +0200

    Update illumination settings in start_hypha_for_squid.py

--- scripts/start_hypha_for_squid.py
+++ scripts/start_hypha_for_squid.py
@@ -196,9 +196,13 @@ def snap(context=None):
         The current frame from the camera transfered to RGB image.
     """
     squidController.camera.send_trigger()
-    squidController.liveController.set_illumination(1,60)
+    squidController.liveController.set_illumination(0,5)
+    if squidController.camera.is_busy():
+        time.sleep(0.005)
     gray_img = squidController.camera.read_frame()
-    squidController.liveController.set_illumination(1,0)
+    squidController.liveController.set_illumination(0,0)
+    if squidController.camera.is_busy():
+        time.sleep(0.005)
     gray_img = np.resize(gray_img, (700, 700))
     #rgb_img = im_processing.gray_to_rgb(gray_img)
     print('The image is snapped.')
