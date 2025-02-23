b89bcef67b9e9fc9489f85cafa30c073120c9457
cccoolll <36098954+cccoolll@users.noreply.github.com>
2025-02-23 19:53:09 +0100

    update squid intergation: Merge: c1ab7c7 79dfcd2 Author: hongquanli <hongquanli@users.noreply.github.com> Date:   Thu Jul 4 11:01:48 2024 -0700
        Merge pull request #67 from sohamazing/master-napari-performance
    
        napari performance

---
+++ software/control/robotic_arm_handler.py
@@ -0,0 +1,7 @@
+
+def move_sample_from_microscope_to_incubator(timeout=60):
+    # Implement the function to move the sample from the microscope to the incubator
+    print("Moving sample from microscope to incubator...")
+    # Add the actual code to control the robotic arm here
+    time.sleep(timeout)
+    print("Sample moved to incubator.")
