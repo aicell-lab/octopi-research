3503db14db573379471c8a40988bef80d0422d32
cccoolll <36098954+cccoolll@users.noreply.github.com>
2025-02-24 18:19:32 +0100

    Implement Hypha control for incubator operations and enhance sample transfer functions

--- software/control/utils_/incubator_handler.py
+++ software/control/utils_/incubator_handler.py
@@ -1,14 +1,38 @@
-from cytomat import Cytomat
 import time
+import asyncio
+from hypha_rpc.sync import connect_to_server
+
+server_url = "http://192.168.2.1:9527"
+service_id = "ws-user-wonderful-ring-09946870/Gqm6TNdfFGYVp3vDfSfp5c:incubator-control"
+def wait_until_not_busy(timeout=50):
+    server = connect_to_server({"server_url": server_url})
+    svc = server.get_service(service_id)
+    
+    while svc.is_busy():
+        time.sleep(1)
+        timeout -= 1
+        if timeout == 0:
+            raise TimeoutError("Timeout reached")
+
 
 def put_sample_from_transfer_station_to_slot(slot=5):
-    c = Cytomat("ttyACM0")
-    c.wait_until_not_busy(timeout=50)
-    c.plate_handler.move_plate_from_transfer_station_to_slot(slot)
-    c.wait_until_not_busy(timeout=50)
+    server = connect_to_server({"server_url": server_url})
+    svc = server.get_service(service_id)
+    wait_until_not_busy(timeout=50)
+    svc.initialize()
+    wait_until_not_busy(timeout=50)
+    svc.put_sample_from_transfer_station_to_slot(slot)
+    wait_until_not_busy(timeout=50)
 
 def get_sample_from_slot_to_transfer_station(slot=5):
-    c = Cytomat("ttyACM0")
-    c.wait_until_not_busy(timeout=50)
-    c.plate_handler.move_plate_from_slot_to_transfer_station(slot)
-    c.wait_until_not_busy(timeout=50)
+    server = connect_to_server({"server_url": server_url})
+    svc = server.get_service(service_id)
+    wait_until_not_busy(timeout=50)
+    svc.initialize()
+    wait_until_not_busy(timeout=50)
+    svc.get_sample_from_slot_to_transfer_station(slot)
+    wait_until_not_busy(timeout=50)
+
+if __name__ == "__main__":
+    put_sample_from_transfer_station_to_slot(5)
+    get_sample_from_slot_to_transfer_station(5)
